3d70a1275b13a7db4bfc34374e130b17d8c120c3
Dorian Macias <dorianrdba@gmail.com>
2024-06-18 22:00:41 -0700

    removed api key

--- src/api/chats.js
+++ src/api/chats.js
@@ -1,18 +1,13 @@
 import { db } from "../db/firestore";
-import { doc, getDoc } from "firebase/firestore";
+import { collection, getDocs, doc, getDoc } from "firebase/firestore";
 
 export const fetchChats = async () => {
   console.log("--- fetching chats ---");
-  console.log("🖥️  db: ", db);
-  const docRef = doc(db, "chats", "mR74JuKuqLsUMAo477o6");
-  const docSnap = await getDoc(docRef);
-
-  if (docSnap.exists()) {
-    console.log("Document data:", docSnap.data());
-  } else {
-    // docSnap.data() will be undefined in this case
-    console.log("No such document!");
-  }
+  const querySnapshot = await getDocs(collection(db, "chats"));
+  
+  querySnapshot.forEach((doc) => {
+    // doc.data() is never undefined for query doc snapshots
+    console.log(doc.id, " => ", doc.data());
+  });
 };
 
-// mR74JuKuqLsUMAo477o6
--- src/db/firestore.js
+++ src/db/firestore.js
@@ -1,21 +1,17 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getFirestore } from "firebase/firestore";
+import { getFirestore } from "firebase/firestore"; // If you are using Firestore
+// import { getDatabase } from "firebase/database"; // If you are using Realtime Database
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBGRO26pUtD_3fJmAcro7dAtFS73qsYdxE",
-  authDomain: "electron-chat-5e0f8.firebaseapp.com",
-  projectId: "electron-chat-5e0f8",
-  storageBucket: "electron-chat-5e0f8.appspot.com",
-  messagingSenderId: "912644837234",
-  appId: "1:912644837234:web:02aecc944e37e309b557eb",
-  measurementId: "G-TVXQL366G1"
+
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+export const db = getFirestore(app); // If using Firestore
 export const analytics = getAnalytics(app);
